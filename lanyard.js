2651605a9b5a409335f46ff0f0b109eea48c0293
2M4U <cybercdn.host@gmail.com>
2022-03-06 19:31:19 +0000

    Update lanyard.js

--- lanyard.js
+++ lanyard.js
@@ -4,16 +4,7 @@ const lanyard = new WebSocket("wss://api.lanyard.rest/socket");
 const currentTime = new Date().getTime();
 var api = {};
 var received = false;
-lanyard.onclose = function () {
-  lanyard.send(
-    JSON.stringify({
-      op: 2,
-      d: {
-        subscribe_to_id: "937051733773938689",
-      },
-    })
-  );
-};
+
 lanyard.onopen = function () {
   lanyard.send(
     JSON.stringify({
@@ -24,16 +15,23 @@ lanyard.onopen = function () {
     })
   );
 };
- function bday(d1, d2) {
-    //console.log(d2)
-    let date1 = new Date(d1);
-    let date2 = new Date(d2);
-    let yearsDiff =  date2.getFullYear() - date1.getFullYear();
-    return yearsDiff;
+function bday(d1, d2) {
+  //console.log(d2)
+  let date1 = new Date(d1);
+  let date2 = new Date(d2);
+  let yearsDiff = date2.getFullYear() - date1.getFullYear();
+  return yearsDiff;
 }
-setInterval(() => {
-  update_presence()
-}, 1000);
+
+var interval = null;
+
+function startStuff(func, time) {
+  interval = setInterval(func, time);
+}
+
+function stopStuff() {
+  clearInterval(interval);
+}
 
 setInterval(() => {
   if (received) {
@@ -53,9 +51,11 @@ lanyard.onmessage = function (event) {
     case "INIT_STATE":
     case "PRESENCE_UPDATE":
       update_presence();
+      stopStuff();
       break;
   }
 };
+
 function update_presence() {
   //console.log(api.d);
   let status;
@@ -78,9 +78,10 @@ function update_presence() {
       break;
   }
   let spotify = api.d.spotify;
-  if (api.d.listening_to_spotify == true) {
+  if (api.d.listening_to_spotify === true) {
     const songLength = spotify.timestamps.end - spotify.timestamps.start;
     const timeElapsed = currentTime - spotify.timestamps.start;
+    startStuff(update_presence(), 1000);
     function msToMinSeconds(millis) {
       var minutes = Math.floor(millis / 60000);
       var seconds = Number(((millis % 60000) / 1000).toFixed(0));
@@ -90,9 +91,13 @@ function update_presence() {
     }
     var artist = `${spotify.artist.split(";")[0].split(",")[0]}`;
     var song = `${spotify.song.split("(")[0]}`;
-    spotifyListening.innerHTML = `sh ./portfolio.sh<br>Name: 2M4U ${status} <sup>(${api.d.discord_user.username})</sup><br>Loc: United Kingdom<br>Repositories: <a href="https://github.com/2m4u">Github</a> <br>Contact Me: comingsoon@2m4u.lol<br>About Me:<br>I'm a ${bday("1994", Date())} year old Full-Stack Developer from Ireland who has a sole passion for coding and computers.<br>I have over 5 years experience in JavaScript, HTML, LSL, Discord Bots and much more.<br>I have alot of open-source projects on my Github.<br>I will eventually add my resume to this site at a later date.<br>
-   <!-- <div class="typer"> --><i class="fab fa-spotify spotify ml-1 mr-1"></i> Listening to <a href="https://open.spotify.com/track/${api.d.spotify.track_id}" target="_blank" class="hover:text-gray-500 text-d-yes" style="color: grey;">${song}</a> by ${artist}<br> ${msToMinSeconds(timeElapsed)} - ${msToMinSeconds(songLength)}<span id="terminal__prompt--cursor"></span><!---</div>-->`;
+    spotifyListening.innerHTML = `sh ./portfolio.sh<br>Name: 2M4U ${status} <sup>(${api.d.discord_user.username})</sup><br><div class="typer"><i class="fab fa-spotify spotify ml-1 mr-1"></i> Listening to <a href="https://open.spotify.com/track/${spotify.track_id}" target="_blank" class="hover:text-gray-500 text-d-yes" style="color: grey;">${song}</a> by ${artist}<br> ${msToMinSeconds(timeElapsed)} - ${msToMinSeconds(songLength)}<span id="terminal__prompt--cursor"></span></div>`;
   } else {
-    spotifyListening.innerHTML = `sh ./portfolio.sh<br>Name: 2M4U ${status} <sup>(${api.d.discord_user.username})</sup><br>Loc: United Kingdom<br>Repositories: <a href="https://github.com/2m4u">Github</a> <br>Contact Me: comingsoon@2m4u.lol<br>About Me:<br>I'm a ${bday("1994", Date())} year old Full-Stack Developer from Ireland who has a sole passion for coding and computers.<br>I have over 5 years experience in JavaScript, HTML, LSL, Discord Bots and much more.<br>I have alot of open-source projects on my Github.<br>I will eventually add my resume to this site at a later date.<span id="terminal__prompt--cursor"></span>`;
+    spotifyListening.innerHTML = `sh ./portfolio.sh<br>Name: 2M4U ${status} <sup>(${
+      api.d.discord_user.username
+    })</sup><br>Loc: United Kingdom<br>Repositories: <a href="https://github.com/2m4u">Github</a> <br>Contact Me: comingsoon@2m4u.lol<br>About Me:<br>I'm a ${bday(
+      "1994",
+      Date()
+    )} year old Full-Stack Developer from Ireland who has a sole passion for coding and computers.<br>I have over 5 years experience in JavaScript, HTML, LSL, Discord Bots and much more.<br>I have alot of open-source projects on my Github.<br>I will eventually add my resume to this site at a later date.<span id="terminal__prompt--cursor"></span>`;
   }
 }
