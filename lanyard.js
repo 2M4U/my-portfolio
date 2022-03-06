800b73c95a97415b862dccc69007841fbbe0a42b
2M4U <cybercdn.host@gmail.com>
2022-03-06 22:19:00 +0000

    Add files via upload

--- lanyard.js
+++ lanyard.js
@@ -1,82 +1,132 @@
-var spotifyListening = document.getElementById("spotifyListening");
+/* Credits to Dustin */
 
-const lanyard = new WebSocket("wss://api.lanyard.rest/socket");
-const currentTime = new Date().getTime();
+function log(...content) {
+  let mainContent = content[0];
+  if (typeof mainContent == "string") content.shift();
+  else mainContent = null;
 
-var api = {};
-var received = false;
-
-lanyard.onclose = function () {
-  lanyard.send(
-    JSON.stringify({
-      op: 2,
-      d: {
-        subscribe_to_id: "937051733773938689",
-      },
-    })
+  console.log(
+    `%cLanyard API%c ${mainContent}`,
+    "padding: 10px; font-size: 1em; line-height: 1.4em; color: white; background: #151515; border-radius: 15px;",
+    "font-size: 15px;",
+    ...content
   );
-};
-lanyard.onopen = function () {
-  lanyard.send(
-    JSON.stringify({
-      op: 2,
-      d: {
-        subscribe_to_id: "937051733773938689",
-      },
-    })
-  );
-};
+}
 
-setInterval(() => {
-  update_presence();
-}, 1000);
-
-setInterval(() => {
-  if (received) {
-    lanyard.send(
-      JSON.stringify({
-        op: 3,
-      })
-    );
-  }
-}, 30000);
-
-lanyard.onmessage = function (event) {
-  received = true;
-  api = JSON.parse(event.data);
-
-  switch (api.t) {
-    case "INIT_STATE":
-    case "PRESENCE_UPDATE":
-      update_presence();
-      break;
-  }
-};
-
-function time(millis) {
-  var minutes = Math.floor(millis / 60000);
-  var seconds = Number(((millis % 60000) / 1000).toFixed(0));
+function time(ms) {
+  let minutes = Math.floor(ms / 60000);
+  let seconds = Number(((ms % 60000) / 1000).toFixed(0));
   return seconds == 60
     ? minutes + 1 + ":00"
     : minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
 }
 
-function update_presence() {
-  console.log(api.d.spotify.timestamps);
+const Op = {
+  Event: 0,
+  Hello: 1,
+  Initalize: 2,
+  Heartbeat: 3,
+};
 
-  let spotify = api.d.spotify;
-  var elapsed = currentTime - spotify.timestamps.start;
+const Event = {
+  InitState: "INIT_STATE",
+  PresenceUpdate: "PRESENCE_UPDATE",
+};
 
-  console.log(time(elapsed));
-  if (api.d.listening_to_spotify == true) {
-    const songLength = spotify.timestamps.end - spotify.timestamps.start;
+let lanyardHeartbeat;
+let spotifyInterval;
 
-    var artist = `${spotify.artist.split(";")[0].split(",")[0]}`;
-    var song = `${spotify.song.split("(")[0]}`;
-    spotifyListening.innerHTML = `sh ./spotify.sh<br>Listening to ${song} by ${artist}<br> ${time(
-      elapsed
-    )} - ${time(songLength)}<span id="terminal__prompt--cursor"></span>`;
-  } else {
-    spotifyListening.innerHTML = `<span id="terminal__prompt--cursor"></span>`;
+document.onreadystatechange = () => {
+  if (document.readyState != "complete") return;
+
+  const Elements = {
+    Spotify: document.getElementById("album"),
+    Name: document.getElementById("name"),
+    Artist: document.getElementById("artist"),
+    Timestamp: document.getElementById("timestamp"),
+  };
+
+  function presenceUpdate(presence) {
+    if (!presence.listening_to_spotify) {
+      Elements.Name.innerText = "Nothing playing";
+      Elements.Artist.innerText = "";
+      Elements.Timestamp.innerText = "";
+      Elements.Spotify.style.visibility = "hidden"
+      clearInterval(spotifyInterval);
+    } else {
+      var artist = `${presence.spotify.artist.split(";")[0].split(",")[0]}`;
+      var song = `${presence.spotify.song.split("(")[0]}`;
+      log(presence.spotify);
+      Elements.Name.innerText = song;
+      Elements.Artist.innerText = artist;
+      //   log(Elements.Spotify.style['background-image'])
+      Elements.Spotify.style.visibility = "visible"
+      Elements.Spotify.src = presence.spotify.album_art_url;
+
+      function updateTimestamp() {
+  
+        Elements.Timestamp.innerText = `${time(
+          new Date().getTime() - presence.spotify.timestamps.start
+        )} - ${time(
+          presence.spotify.timestamps.end - presence.spotify.timestamps.start
+        )}`;
+      }
+
+      clearInterval(spotifyInterval);
+      spotifyInterval = setInterval(() => updateTimestamp(), 900);
+      updateTimestamp();
+    }
   }
-}
+
+  function connect() {
+    const socket = new WebSocket("wss://api.lanyard.rest/socket");
+
+    function send(op, d) {
+      if (socket.readyState != socket.OPEN) return;
+      return socket.send(JSON.stringify({ op, d }));
+    }
+
+    socket.onopen = () => {
+      log("Connected to socket");
+    };
+
+    socket.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+
+      switch (data.op) {
+        case Op.Hello: {
+          log("Got hello op");
+
+          lanyardHeartbeat = setInterval(
+            () => send(Op.Heartbeat),
+            data.d.heartbeat_interval
+          );
+
+          send(Op.Initalize, { subscribe_to_id: "937051733773938689" });
+
+          break;
+        }
+        case Op.Event: {
+          switch (data.t) {
+            case Event.InitState:
+            case Event.PresenceUpdate: {
+              presenceUpdate(data.d);
+              break;
+            }
+          }
+
+          break;
+        }
+      }
+    };
+
+    socket.onclose = (event) => {
+      clearInterval(lanyardHeartbeat);
+      clearInterval(spotifyInterval);
+      log("Socket closed", event.reason, event.code);
+      connect();
+    };
+  }
+
+  connect();
+};
